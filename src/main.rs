9c5d70fb998ac60d37120b9f722c4d044078148a
Uriel Chavez <sacariasdaem@gmail.com>
2024-02-02 02:37:31 +0000

    programa funcional de expresiones regulares, funciona v1.0

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,32 @@
 use regex::Regex;
-use regex::Replacer;
 use std::io;
 use std::fs;
 
 fn main() {
     loop{
         let mut input_user = String::new();
-        print!("Ingrese regular expresion o presione <salir> y <enter> para cerrar programa\n");
+        print!("\nIngrese su regular expresion o ingrese '***' y presione <enter> para cerrar programa\n");
         io::stdin().read_line(&mut input_user).unwrap();
         input_user = input_user.trim().to_string();
+        print!("\n");
 
-        if input_user == "salir" { break; }
+        if input_user == "***" { break; }
 
         let path_csv= "movies.csv";
-        let mut contenido_csv = fs::read_to_string(path_csv).unwrap();
-        let regular_e = Regex::new(&input_user).unwrap();
+        let contenido_csv = fs::read_to_string(path_csv).unwrap();
+        let regular_e = Regex::new(&input_user).unwrap_or(Regex::new(r".").unwrap());
+        let clone_contenido_csv = contenido_csv.clone();
+        let rows_de_contenido = clone_contenido_csv.lines();
 
-        loop {
-            let cont_csv = contenido_csv.clone();
-            let captura_regular_e = regular_e.captures(&cont_csv);
-
-            if captura_regular_e.is_none(){break;}
-            
-            let captura = captura_regular_e.unwrap();
-            print!("{:?}\n", captura);
-            let c = captura.get(0).unwrap().as_str().to_string();
-            contenido_csv.replace_range(cont_csv.find(&c)..c.len(), replace_with)
-
+        for line in rows_de_contenido.clone(){
+            let captura_regular_e = regular_e.captures(line);
+                
+            if captura_regular_e.is_some(){
+                print!("{}\n", line);
+            }
         }
+
+        print!("\n---------------------------------------------------------------------------------------------------------")
     }
 }
     
