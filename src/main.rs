f73348ef7d34e944bd6a3b4cad460b95d8ebb8d1
Uriel Chavez <sacariasdaem@gmail.com>
2024-02-07 02:05:53 +0000

    Agregado otro archivo de texto <liners.txt>

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
         if input_user == "***" { break; }
 
         //let path_csv= "movies.csv";
-        let path_liners = "liners_phone";
+        let path_liners = "liners.txt";
         let contenido_csv = fs::read_to_string(path_liners).unwrap();
         let regular_e = Regex::new(&input_user).unwrap_or(Regex::new(r".").unwrap());
         let clone_contenido_csv = contenido_csv.clone();
@@ -26,17 +26,17 @@ fn main() {
             if captura_regular_e.is_some(){
                 print!("{}\n", line);
                 loop{
-                    let regular_e2 = Regex::new(&input_user).unwrap_or(Regex::new(r".").unwrap());
-                    let captura_regular_e2 = regular_e2.captures(&manipulacion_line);
+                    let captura_regular_e = regular_e.captures(&manipulacion_line);
 
-                    if captura_regular_e2.is_none(){break;}
+                    if captura_regular_e.is_none(){break;}
 
-                    let captura = captura_regular_e2.unwrap().get(0).unwrap().as_str();
+                    let captura = captura_regular_e.unwrap().get(0).unwrap().as_str();
 
                     if captura == "" {break;}
-                    let replace_line= manipulacion_line.replacen(captura, "", 1);
+
+                    let replacen_line = manipulacion_line.replacen(captura, "", 1);
                     print!(">{} ", captura);
-                    manipulacion_line = replace_line;
+                    manipulacion_line = replacen_line;
                 }
                 print!("\n\n");
             }
